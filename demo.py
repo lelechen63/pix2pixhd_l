4d8b47801171d3f382cfe836cfbd21b426af2f8a
lelechen63 <lchen63@cs.rochester.edu>
2019-05-31 15:52:59 -0700

    add demo

--- demo.py
+++ demo.py
@@ -44,88 +44,98 @@ if not opt.engine and not opt.onnx:
         print(model)
 else:
     from run_engine import run_trt_engine, run_onnx
-    
-in_img_tensor = inst_tensor = feat_tensor = 0
-     
-### input A (label maps)
-
-# input 1 : front image
-input_image = opt.identity_image.replace('.jpg','_512.jpg')
-# input 2 (garment parsing)
-garment =  input_image.replace('.jpg', '_parsing1.png')
-A = Image.open(garment)        
-# A =  A.transpose(Image.FLIP_LEFT_RIGHT)
-params = get_params(opt, A.size)
-if opt.label_nc == 0:
-    transform_A = get_transform(opt, params)
-    A = A.convert('RGB')
-    # A = transforms.functional.affine(A, params['angle'], params['translate'], params['scale'], params['shear'] )
-    in_tensor = transform_A(A)
-    
-else:
-    transform_A = get_transform(opt, params, method=Image.NEAREST, normalize=False)
-    # A = transforms.functional.affine(A, params['angle'], params['translate'], params['scale'], params['shear'] )
-    in_tensor = transform_A(A) * 255.0
-
-B = Image.open(input_image).convert('RGB')
-# params = get_params(self.opt, B.size)
-transform_B = get_transform(opt, params)  
-# B = transforms.functional.affine(B, params['angle'], params['translate'], params['scale'], params['shear'] )    
-in_img_tensor = transform_B(B)
-
-# gt view can be back size view or side view
-
-gt_image = opt.pose_image.replace('.jpg','_512.jpg')
-
-C = Image.open(gt_image).convert('RGB')
-# params = get_params(self.opt ,C.size)
-
-transform_C = get_transform(opt, params)
-# C = transforms.functional.affine(C, params['angle'], params['translate'], params['scale'], params['shear'] )       
-out_img_tensor = transform_C(C)
-
-# gt garment parsing
-gt_garment = gt_image.replace('.jpg', '_parsing1.png')
-gt_garment = Image.open(gt_garment)
-
-# gt_garment =  gt_garment.transpose(Image.FLIP_LEFT_RIGHT)        
-# params = get_params(self.opt, gt_garment.size)
-if opt.label_nc == 0:
-    transform_A = get_transform(opt, params)
-    gt_garment =  gt_garment.convert('RGB')
-    # gt_garment =  transforms.functional.affine(gt_garment, params['angle'], params['translate'], params['scale'], params['shear'] ) 
-    A_tensor = transform_A(gt_garment)
-else:
-    transform_D = get_transform(opt, params, method=Image.NEAREST, normalize=False)
-    # gt_garment = transforms.functional.affine(gt_garment, params['angle'], params['translate'], params['scale'], params['shear'] ) 
-    gt_tensor = transform_D(gt_garment) * 255.0
-
-data = {'input_parsing': in_tensor.unsqueeze(0),  'input_image': in_img_tensor.unsqueeze(0),
-    'gt_parsing': gt_tensor.unsqueeze(0), 'gt_image': out_img_tensor.unsqueeze(0), 'input_path': input_image, 'gt_path':gt_image}
-
-if opt.data_type == 16:
-    data['input_parsing'] = data['input_parsing'].half()
-    data['gt_parsing']  = data['gt_parsing'].half()
-elif opt.data_type == 8:
-    data['input_parsing'] = data['input_parsing'].uint8()
-    data['gt_parsing']  = data['gt_parsing'].uint8()
 
 
-
-
-minibatch = 1     
-generated = model.inference(data['input_parsing'], data['input_image'], data['gt_parsing'],data['gt_image'])
+back_img = '/home/lchen63/data_test/lele_b.jpg'
+front_img = ['/home/lchen63/data_test/lele_f.jpg',
+'/home/lchen63/data_test/Zhong_wg.jpg',
+'/home/lchen63/data_test/Shuang1.jpg',
+'/home/lchen63/data_test/yuxin_wg.jpg'
+]
+for jj in front_img:
+    opt.front_img = jj
+    opt.back_img = back_img
     
 
-# visuals = OrderedDict([('input_label', util.tensor2label(data['label'][0], opt.label_nc)),
-                       # ('synthesized_image', util.tensor2im(generated.data[0]))])
-visuals = OrderedDict([('input_image', util.tensor2im(data['input_image'][0])),
-                                ('input_parsing', util.tensor2label(data['input_parsing'][0],  opt.label_nc)),
-                                ('gt_label', util.tensor2label(data['gt_parsing'][0], opt.label_nc)),
-                                ('synthesized_image', util.tensor2im(generated.data[0])),
-                                ('real_image', util.tensor2im(data['gt_image'][0]))])
-img_path = data['gt_path']
-print('process image... %s' % img_path)
-visualizer.save_images(webpage, visuals, [img_path])
+    in_img_tensor = inst_tensor = feat_tensor = 0     
+    ### input A (label maps)
+    # input 1 : front image
+    input_image = opt.identity_image.replace('.jpg','_512.jpg')
+    # input 2 (garment parsing)
+    garment =  input_image.replace('.jpg', '_parsing1.png')
+    A = Image.open(garment)        
+    # A =  A.transpose(Image.FLIP_LEFT_RIGHT)
+    params = get_params(opt, A.size)
+    if opt.label_nc == 0:
+        transform_A = get_transform(opt, params)
+        A = A.convert('RGB')
+        # A = transforms.functional.affine(A, params['angle'], params['translate'], params['scale'], params['shear'] )
+        in_tensor = transform_A(A)
+        
+    else:
+        transform_A = get_transform(opt, params, method=Image.NEAREST, normalize=False)
+        # A = transforms.functional.affine(A, params['angle'], params['translate'], params['scale'], params['shear'] )
+        in_tensor = transform_A(A) * 255.0
+
+    B = Image.open(input_image).convert('RGB')
+    # params = get_params(self.opt, B.size)
+    transform_B = get_transform(opt, params)  
+    # B = transforms.functional.affine(B, params['angle'], params['translate'], params['scale'], params['shear'] )    
+    in_img_tensor = transform_B(B)
+
+    # gt view can be back size view or side view
+
+    gt_image = opt.pose_image.replace('.jpg','_512.jpg')
+
+    C = Image.open(gt_image).convert('RGB')
+    # params = get_params(self.opt ,C.size)
+
+    transform_C = get_transform(opt, params)
+    # C = transforms.functional.affine(C, params['angle'], params['translate'], params['scale'], params['shear'] )       
+    out_img_tensor = transform_C(C)
+
+    # gt garment parsing
+    gt_garment = gt_image.replace('.jpg', '_parsing1.png')
+    gt_garment = Image.open(gt_garment)
+
+    # gt_garment =  gt_garment.transpose(Image.FLIP_LEFT_RIGHT)        
+    # params = get_params(self.opt, gt_garment.size)
+    if opt.label_nc == 0:
+        transform_A = get_transform(opt, params)
+        gt_garment =  gt_garment.convert('RGB')
+        # gt_garment =  transforms.functional.affine(gt_garment, params['angle'], params['translate'], params['scale'], params['shear'] ) 
+        A_tensor = transform_A(gt_garment)
+    else:
+        transform_D = get_transform(opt, params, method=Image.NEAREST, normalize=False)
+        # gt_garment = transforms.functional.affine(gt_garment, params['angle'], params['translate'], params['scale'], params['shear'] ) 
+        gt_tensor = transform_D(gt_garment) * 255.0
+
+    data = {'input_parsing': in_tensor.unsqueeze(0),  'input_image': in_img_tensor.unsqueeze(0),
+        'gt_parsing': gt_tensor.unsqueeze(0), 'gt_image': out_img_tensor.unsqueeze(0), 'input_path': input_image, 'gt_path':gt_image}
+
+    if opt.data_type == 16:
+        data['input_parsing'] = data['input_parsing'].half()
+        data['gt_parsing']  = data['gt_parsing'].half()
+    elif opt.data_type == 8:
+        data['input_parsing'] = data['input_parsing'].uint8()
+        data['gt_parsing']  = data['gt_parsing'].uint8()
+
+
+
+
+    minibatch = 1     
+    generated = model.inference(data['input_parsing'], data['input_image'], data['gt_parsing'],data['gt_image'])
+        
+
+    # visuals = OrderedDict([('input_label', util.tensor2label(data['label'][0], opt.label_nc)),
+                           # ('synthesized_image', util.tensor2im(generated.data[0]))])
+    visuals = OrderedDict([('input_image', util.tensor2im(data['input_image'][0])),
+                                    ('input_parsing', util.tensor2label(data['input_parsing'][0],  opt.label_nc)),
+                                    ('gt_label', util.tensor2label(data['gt_parsing'][0], opt.label_nc)),
+                                    ('synthesized_image', util.tensor2im(generated.data[0])),
+                                    ('real_image', util.tensor2im(data['gt_image'][0]))])
+    img_path = data['gt_path']
+    print('process image... %s' % img_path)
+    visualizer.save_images(webpage, visuals, [img_path])
 
 webpage.save()
