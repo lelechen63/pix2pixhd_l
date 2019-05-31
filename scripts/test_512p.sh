99de31bca6c849020114b3c5e1dfeef714a9b324
lelechen63 <lchen63@cs.rochester.edu>
2019-05-31 16:36:08 -0700

    add script

--- scripts/test_512p.sh
+++ scripts/test_512p.sh
@@ -1,3 +1,5 @@
 ################################ Testing ################################
 # labels only
-python demo.py --train='test' --name label2city_512p
+#python demo.py  --name label2city_512p
+
+python test.py --name deepfashion_man
--- scripts/train_512p_multigpu.sh
+++ scripts/train_512p_multigpu.sh
@@ -1,4 +1,5 @@
 ######## Multi-GPU training example #######
-python train.py --continue_train  --name deepfashion_man --batchSize 64 --gpu_ids 0,1,2,3,4,5,6
+#python train.py --continue_train  --name deepfashion_man --batchSize 64 --gpu_ids 0,1,2,3,4,5,6
 
+python train.py --name deepfashion_man --batchSize 64 --gpu_ids 0,1,2,3,4,5,6
 
