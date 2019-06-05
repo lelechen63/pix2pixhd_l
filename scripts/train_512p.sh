4bed0c1c95183dd1c78e6fd9550cefae6757cfcd
lelechen63 <lchen63@cs.rochester.edu>
2019-06-04 18:15:49 -0700

    add sun

--- scripts/train_512p.sh
+++ scripts/train_512p.sh
@@ -1,4 +1,4 @@
 ### Using labels only
-python train.py --name label2city_512p --dataset densepose --batchSize 16
+#python train.py --name label2city_512p --dataset densepose --batchSize 16
 
-python train.py --name syn512p --dataroot /data/syn --dataset syn
+python train.py --name syn512p --dataroot /data/syn --dataset syn --batchSize 16
