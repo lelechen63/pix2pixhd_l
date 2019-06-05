6efcc23c2a1546200bc792e72b19b75084f9901f
lelechen63 <lchen63@cs.rochester.edu>
2019-06-05 14:19:33 -0700

    g

--- scripts/test_512p.sh
+++ scripts/test_512p.sh
@@ -1,5 +1,5 @@
 ################################ Testing ################################
 # labels only
-python demo_syn.py  --name syn512p
+python demo_syn.py  --name syn512p --model noback
 
 # python test.py --name syn512p --dataroot /data/syn --model noback --dataset syn --serial_batches
