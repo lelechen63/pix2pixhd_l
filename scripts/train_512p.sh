b92cef9ae9578bad5d996adccd9bb459b144398a
lelechen63 <lchen63@cs.rochester.edu>
2019-06-05 11:58:23 -0700

    add musk to real data

--- scripts/train_512p.sh
+++ scripts/train_512p.sh
@@ -1,4 +1,4 @@
 ### Using labels only
-#python train.py --name label2city_512p --dataset densepose --batchSize 16
+python train.py --name label2city_512p --dataset densepose --batchSize 20 --gpu_ids 5,7
 
-python train.py --name syn512p --dataroot /data/syn --model noback --dataset syn --batchSize 32  --gpu_ids 1,2,3,4
+# python train.py --name syn512p --dataroot /data/syn --model noback --dataset syn --batchSize 32  --gpu_ids 1,2,3,4
