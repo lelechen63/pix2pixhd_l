b009b4ec91ae838ca5a3c49cc27b98ed61053165
lelechen63 <lchen63@cs.rochester.edu>
2019-06-05 11:25:34 -0700

    add demo syn

--- scripts/train_512p.sh
+++ scripts/train_512p.sh
@@ -1,4 +1,4 @@
 ### Using labels only
 #python train.py --name label2city_512p --dataset densepose --batchSize 16
 
-python train.py --name syn512p --dataroot /data/syn --model noback --dataset syn --batchSize 32 --continue_train --gpu_ids 1,2,3,4
+python train.py --name syn512p --dataroot /data/syn --model noback --dataset syn --batchSize 32  --gpu_ids 1,2,3,4
